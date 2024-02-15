94d6a84153887a19155bac3ac4e9ac091c303cd6
Anshu <anshu.possible@gmail.com>
2024-02-15 17:20:37 +0530

    added react element

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+
+const elem = <span>This is React Element</span>
+
 const Title = () => (
   <h1 className="head">
-    Namste React using JSX 🚀
+  {elem}
+     Namste React using JSX 🚀
   </h1>
+  
 );
 const number = 5000;
 
